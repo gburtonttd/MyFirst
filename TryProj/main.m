d98ffa154600b19160557c422223b72d6566d66e
Chau Doan <cdoan@tatertotdesigns.com>
2013-06-27 12:34:15 -0700

    This is my first commit. Not sure if the cdoan folders of Interface State and Schemes is necessary to commit but decided to include all anyway.

---
+++ TryProj/main.m
@@ -0,0 +1,22 @@
+//
+//  main.m
+//  TryProj
+//
+//  Created by Gina Burton on 6/27/13.
+//  Copyright (c) 2013 Gina Burton. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[])
+{
+
+    @autoreleasepool {
+        
+        // insert code here...
+        NSLog(@"Hello, World!");
+        
+    }
+    return 0;
+}
+
