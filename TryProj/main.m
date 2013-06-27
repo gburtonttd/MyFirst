c5426ab4c4be1f9ef1766924a189c95b4cf00f71
Chau Doan <cdoan@tatertotdesigns.com>
2013-06-27 13:16:51 -0700

    Another attempt to push files to GitHub

--- TryProj/main.m
+++ TryProj/main.m
@@ -14,7 +14,7 @@ int main(int argc, const char * argv[])
     @autoreleasepool {
         
         // insert code here...
-        NSLog(@"Hello, World!");
+        NSLog(@"Hello, World etc, etc!");
         
     }
     return 0;
