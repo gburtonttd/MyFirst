b140dcd09fb403df638ddf42a8d8f51094e78d4e
Chau Doan <cdoan@tatertotdesigns.com>
2013-06-28 14:10:31 -0700

    added code to calculate rate from Rate class but  not working yet.

---
+++ TryProj/Rate.h
@@ -0,0 +1,13 @@
+//
+//  Rate.h
+//  TryProj
+//
+//  Created by Gina Burton on 6/28/13.
+//  Copyright (c) 2013 Gina Burton. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface Rate : NSObject
+
+@end
---
+++ TryProj/Rate.m
@@ -0,0 +1,13 @@
+//
+//  Rate.m
+//  TryProj
+//
+//  Created by Gina Burton on 6/28/13.
+//  Copyright (c) 2013 Gina Burton. All rights reserved.
+//
+
+#import "Rate.h"
+
+@implementation Rate
+
+@end
